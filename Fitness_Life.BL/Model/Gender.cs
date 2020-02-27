f244374a239584ab5c547241e10f448430e5b1bb
Aleksandr Davidenko <alex1190119022@gmail.com>
2020-02-27 22:25:29 +0200

    Add a new user

---
+++ Fitness_Life.BL/Model/Gender.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+namespace Fitness_Life.BL.Model
+{
+    class Gender
+    {
+        public string Name { get; }
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+            Name = name;
+        }
+        public override string ToString()
+        {
+               return Name;
+        }    
+    }
+}
---
+++ Fitness_Life.BL/Model/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace Fitness_Life.BL.Model
+{
+    class User
+    {
+        public string Name { get; }
+        public Gender Gender { get; }
+        public DateTime BirthDate { get; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public User(string name, 
+                   Gender gender, 
+                   DateTime birthDate, 
+                   double weight, 
+                   double height)
+        {
+            #region
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пользователя не может быть пустым или null", nameof(name));
+            }
+            if (gender == null)
+            {
+                throw new ArgumentNullException("Пол не может быть пустым или null", nameof(gender));
+            }
+            if (birthDate < DateTime.Parse("01.01.1900") || birthDate < DateTime.Now)
+            {
+                throw new ArgumentException("Невозможная дата рождения", nameof(birthDate));
+            }
+            if (weight <= 0)
+            {
+                throw new ArgumentException("Невозможный вес", nameof(weight));
+            }
+            if (height <= 0)
+            {
+                throw new ArgumentException("Рост не может быть меньше, или равен 0", nameof(height));
+            }
+            #endregion
+            Name = name;
+            Gender = gender;
+            BirthDate = birthDate;
+            Weight = weight;
+            Height = height;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
